3f2fb1e8ccc1c8e70783cd9cb6e938fd30e4f8b4
sashatytko <sashatytko@gmail.com>
2023-02-09 15:47:53 +0200

    Implemented tip calculation logic

--- app/src/main/java/edu/tytko/tiptime/MainActivity.kt
+++ app/src/main/java/edu/tytko/tiptime/MainActivity.kt
@@ -2,10 +2,35 @@ package edu.tytko.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import edu.tytko.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val cost = binding.costOfService.text
+        if (cost == null || cost.isEmpty()) {
+            binding.tipResult.text = ""
+            return
+        }
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.2
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = cost.toString().toDouble() * tipPercentage
+        if (binding.roundUpSwitch.isChecked) {
+            tip = kotlin.math.ceil(tip)
+        }
+        binding.tipResult.text =
+            getString(R.string.tip_amount, NumberFormat.getCurrencyInstance().format(tip))
     }
 }
